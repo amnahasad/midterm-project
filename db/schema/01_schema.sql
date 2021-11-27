b68925563bea3e68bc261704451feb191038ae0d
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-27 18:35:10 -0500

    Added tables and values to DB

---
+++ db/schema/01_schema.sql
@@ -0,0 +1,48 @@
+-- Drop and recreate customers table (Example)
+
+DROP TABLE IF EXISTS customers CASCADE;
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quantity INTEGER NOT NULL DEFAULT 1,
+  comments TEXT,
+  total INTEGER NOT NULL,
+  confirmation INTEGER NOT NULL,
+  wait_time TIMESTAMP,
+  order_ready BOOLEAN,
+  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  order_item_id INTEGER REFERENCES order_items(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS meals CASCADE;
+CREATE TABLE meals (
+  id SERIAL PRIMARY KEY NOT NULL,
+  main_course VARCHAR(255),
+  side_dish VARCHAR(255),
+  baked_goods VARCHAR(255),
+  house_special VARCHAR(255),
+  price INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS drinks CASCADE;
+CREATE TABLE drinks (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS order_items CASCADE;
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  meal_id INTEGER REFERENCES meals(id) ON DELETE CASCADE,
+  drink_id INTEGER REFERENCES customers(id) ON DELETE CASCADE
+);
+
+
