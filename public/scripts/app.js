1be17feab1e421d7e18fae1d32240e74a930d25e
belalelmi <belalelmi@gmail.com>
2021-12-01 11:24:57 -0500

    page styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,19 @@
 // Client facing scripts here
 
-const createNewOrder = function() {
+const createNewOrder = function () {
 
 };
 
 
-$(document).ready(function() {
+$(document).ready(function () {
   // $('#drinks').on('submit', function(event) {
   //   console.log("drinks");
 
   // })
 
-  const createDrinkElement = function(drink) {
+  const createDrinkElement = function (drink) {
 
-      <div id="drink" data="<%=drink.name%>"> ${drink.name}, ${drink.description}, ${drink.price}
+    <div id="drink" data="<%=drink.name%>"> ${drink.name}, ${drink.description}, ${drink.price}
       <button id="drinks-button" style="
       background-color: #E887BB;
       color: #FFFFFF;
@@ -21,7 +21,7 @@ $(document).ready(function() {
       padding-top: 3px;
       padding-bottom: 3px;
       font-size: small;"
-      type="submit">Add item</button>
+        type="submit">Add item</button>
     </div>
 
   }
